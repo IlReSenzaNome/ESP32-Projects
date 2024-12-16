dc1ebd6b0b3d7b33276453f9a7503209391057b7
IlReSenzaNome <alexisvivas3@gmail.com>
2024-12-16 14:48:46 -0500

    Manejo de pantalla OLED

--- 5. Oled display/main.py
+++ 5. Oled display/main.py
@@ -1,2 +1,34 @@
 import machine
+import ssd1306
 import time
+
+# Configura los pines para I2C
+i2c = machine.I2C(0, scl=machine.Pin(22), sda=machine.Pin(21))
+
+# Inicializa la pantalla OLED
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+
+while True: 
+    oled.fill(0)
+    oled.text("Texto de prueba", 0, 0)
+    oled.show()
+
+# # Configura el pin del LED incorporado
+# led = machine.Pin(2, machine.Pin.OUT)
+
+# # Muestra "Hola Mundo" en la pantalla OLED
+# oled.fill(0)
+# oled.text('Hola Mundo', 0, 0)
+# oled.show()
+
+# # Enciende el LED
+# led.on()
+
+# # Espera 10 segundos
+# time.sleep(10)
+
+# # Apaga el LED
+# led.off()
