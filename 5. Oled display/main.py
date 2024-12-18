a96608da132b83bcfe27dc1604c42784ea4e6022
IlReSenzaNome <alexisvivas3@gmail.com>
2024-12-18 15:08:35 -0500

    Update Oled Display

--- 5. Oled display/main.py
+++ 5. Oled display/main.py
@@ -1,34 +1,34 @@
-import machine
-import ssd1306
-import time
+# Display Image & text on I2C driven ssd1306 OLED display
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import framebuf
 
-# Configura los pines para I2C
-i2c = machine.I2C(0, scl=machine.Pin(22), sda=machine.Pin(21))
+WIDTH  = 128                                            # oled display width
+HEIGHT = 32                                             # oled display height
 
-# Inicializa la pantalla OLED
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
+print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
+print("I2C Configuration: "+str(i2c))                   # Display I2C config
 
 
-while True: 
-    oled.fill(0)
-    oled.text("Texto de prueba", 0, 0)
-    oled.show()
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
 
-# # Configura el pin del LED incorporado
-# led = machine.Pin(2, machine.Pin.OUT)
+# Raspberry Pi logo as 32x32 bytearray = 
 
-# # Muestra "Hola Mundo" en la pantalla OLED
-# oled.fill(0)
-# oled.text('Hola Mundo', 0, 0)
-# oled.show()
+buffer = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
 
-# # Enciende el LED
-# led.on()
+# Load the raspberry pi logo into the framebuffer (the image is 32x32)
+fb = framebuf.FrameBuffer(buffer, 32, 32, framebuf.MONO_HLSB)
 
-# # Espera 10 segundos
-# time.sleep(10)
+# Clear the oled display in case it has junk on it.
+oled.fill(0)
 
-# # Apaga el LED
-# led.off()
+# Blit the image from the framebuffer to the oled display
+oled.blit(fb, 0, 0)
+
+# Add some text
+# oled.text("Raspberry Pi",5,5)
+# oled.text("Pico",5,15)
+
+# Finally update the oled display so the image & text is displayed
+oled.show()
